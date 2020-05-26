64ae8a7596faca92260bd619ac749fc9d758536f
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2020-05-26 16:42:51 -0500

    Add function

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.Saludo = functions.https.onRequest((request, response) => {
+    response.send("Hola visitante");
+});
+
+exports.SendEmail = functions.firestore
+    .document('users/{usersId}')
+    .onCreate((change, context) => { //https.onRequest((request, response) => {
+
+        let information = change.data();
+
+        const nodemailer = require("nodemailer");
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'bictia.cohorte2@gmail.com', // generated ethereal user
+                pass: 'B1ct142020', // generated ethereal password
+            },
+        });
+
+        let emailOptions = {
+            from: '"Curso Bictia 🤖🏫" <bictia.cohorte2@gmail.com>',
+            to: information.email,
+            subject: 'Registro exitoso',
+            text: 'Hello ✔',
+            html: '<b>Bienvenido a '+information.name+' nuestro fantabulosa aplicación</b><br>Soy un texto normal, probando nodemailer<br><img src="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_960_720.jpg" alt="Imagen para probar">'
+        };
+        return transporter.sendMail(emailOptions).then((data) => {
+            resolve(data);
+            return;
+        }).catch((error) => {
+            reject(error);
+            return;
+        });
+    });
